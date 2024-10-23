5a10606584e19562ef504bc9439102902c28e5fe
Rohitkewat11 <133211337+Rohitkewat11@users.noreply.github.com>
2024-10-23 12:04:52 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
+// import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Page from './components/page';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Page/>
+     <App /> 
   </React.StrictMode>
 );
 
